3edb495c4f680db88effff44e370d80d10bf689a
laurengodwin <lauren.godwin@umconnect.umt.edu>
2017-10-26 10:57:58 -0600

    figuring out sketch.js

---
+++ Week 3/hw-3/empty-example/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+
+  createCanvas( 600, 400);
+  background( 'pink' );
+}
+// Declare the draw function
+function draw() {
+
+    // tell p5 to print the text string "Hello World!"
+    text( "Hello World!", 20, 30 );
+}
---
+++ Week 5/cursor.js
@@ -0,0 +1,12 @@
+// making mouse object
+strokeWeight(3);
+stroke(57, 0, 214);
+fill(0, 210, 247);
+
+drawfunction() {
+
+    background(255, 255, 255);
+
+    ellipse(mouseX, mouseY, 30, 30);
+
+};
