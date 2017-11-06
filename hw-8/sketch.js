36b5f314784420462f8031122f17e74743790685
laurengodwin <lauren.godwin@umconnect.umt.edu>
2017-11-06 12:53:00 -0700

    hw-8

--- hw-8/sketch.js
+++ hw-8/sketch.js
@@ -1,7 +1,32 @@
+let rows, x_space;
+let cols, y_space;
+
 function setup() {
-  // put setup code here
+	createCanvas(windowWidth, windowHeight);
+	frameRate( 100 );
+
+	x_space = 50;
+	rows = floor( width / x_space );
+	y_space = 50;
+	cols = floor( height / y_space );
 }
 
+
+
 function draw() {
-  ellipse(50,50,80,80);
+	background( 'rgb(150, 178, 245)' );
+
+	noStroke();
+
+	// making ‘x’ to a dummy value.
+	let x = 1;
+	let y = 1;
+	while ( x != 0 ){
+		x = floor(random() * rows);
+		y = floor(random() * cols);
+
+// giving the triangles color
+		fill( random(255), random(255), random(255) );
+		ellipse( x*x_space, y*y_space, random(10, 50,) );
+	}
 }
