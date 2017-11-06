27c710b92dca706278976f29ce09516fa1988ba4
laurengodwin <lauren.godwin@umconnect.umt.edu>
2017-11-06 12:31:55 -0700

    hw-7

---
+++ hw-7/HW-7/readme.md
@@ -0,0 +1,13 @@
+Lauren Godwin, 50
+
+[Live Sketch Link](Your Live Link)
+
+
+# HW 7 | Describe then Alter the Bouncing Ball
+
+## Description of What the Original Code is Doing
+<p>It works this way due to the alogrithms and variable codes. The first ten lines controll the circles size and speed. Delta controls the angle, scale controls the speed. ball x and y control the starting location. createCanvas and background create the setting for this sketch. The draw function is where all the action happens for this code. The function pressed is the function that makes the circle move angles when you click on the canvas.</p>
+
+
+## How did you alter the sketch?
+<p>First off I changed the background color to bright yellow. Then I changed the screen to fullsize. Next I changed the ball width to 100. Then i changed the circle fill color to red. Then I added another circle to create an alternating colored look.</p>
---
+++ hw-7/HW-7/sketch.js
@@ -0,0 +1,55 @@
+
+
+var ball = {};
+ball.width = 100;
+ball.x = 10;
+ball.y = 10;
+ball.delta_x = 1;
+ball.delta_y = 1;
+ball.scale_x = 1;
+ball.scale_y = 1;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background('rgb(255,250,0)');
+}
+
+
+
+function draw() {
+
+    ball.x += ball.delta_x * ball.scale_x;
+    ball.y += ball.delta_y * ball.scale_y;
+
+
+    if (ball.x >= width || ball.x <= 0) {
+        ball.delta_x = -1 * ball.delta_x;
+    }
+    if (ball.y >= height || ball.y <= 0) {
+        ball.delta_y = -1 * ball.delta_y;
+    }
+
+    fill('rgb(255,0,0)');
+    ellipse(ball.x, ball.y, ball.width, ball.width);
+
+    ball.x += ball.delta_x * ball.scale_x;
+    ball.y += ball.delta_y * ball.scale_y;
+
+
+    if (ball.x >= width || ball.x <= 0) {
+        ball.delta_x = -1 * ball.delta_x;
+    }
+    if (ball.y >= height || ball.y <= 0) {
+        ball.delta_y = -1 * ball.delta_y;
+    }
+
+    fill('rgb(145,0,255)');
+    ellipse(ball.x, ball.y, ball.width, ball.width);
+
+
+}
+
+function mousePressed() {
+    ball.scale_x = map(mouseX, 0, width, 0.5, 10);
+    ball.scale_y = map(mouseY, 0, height, 0.5, 10);
+}
